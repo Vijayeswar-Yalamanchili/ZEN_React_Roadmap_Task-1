e70c7942fa492659e203b3df004d4a92a8ea5825
Vijayeswar-Yalamanchili <vijayeswar_y@yahoo.com>
2023-12-21 00:47:44 +0530

    completed

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -11,14 +11,14 @@ function Cards({cardData}) {
                     <h6 className="card-price text-center">${cardData.price}<span className="period">/month</span></h6>
                     <hr/>
                     <ul className="fa-ul">
-                        <li><span className="fa-li"><i className="fas fa-check"></i></span>{cardData.user}</li>
-                        <li><span className="fa-li"><i className="fas fa-check"></i></span>{cardData.storage}</li>
-                        <li><span className="fa-li"><i className="fas fa-check"></i></span>{cardData.publicProjects}</li>
-                        <li><span className="fa-li"><i className="fas fa-check"></i></span>{cardData.communityAccess}</li>
-                        <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{cardData.privateProjects}</li>
-                        <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{cardData.phoneSupport}</li>
-                        <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{cardData.subDomian}</li>
-                        <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{cardData.reports}</li>
+                        <li className={ cardData.isUser ? "" : "text-muted"}><span className="fa-li"><i className= { cardData.isUser ? "fas fa-check" : "fas fa-times"}></i></span>{cardData.plan !== "FREE" ? <b>{cardData.user}</b> : <>{cardData.user}</>}</li>
+                        <li className={ cardData.isStorage ? "" : "text-muted"}><span className="fa-li"><i className={ cardData.isStorage ? "fas fa-check" : "fas fa-times"}></i></span>{cardData.storage}</li>
+                        <li className={ cardData.isPublicProjects ? "" : "text-muted"}><span className="fa-li"><i className={ cardData.isPublicProjects ? "fas fa-check" : "fas fa-times"}></i></span>{cardData.publicProjects}</li>
+                        <li className={ cardData.isCommunityAccess ? "" : "text-muted"}><span className="fa-li"><i className={ cardData.isCommunityAccess ? "fas fa-check" : "fas fa-times"}></i></span>{cardData.communityAccess}</li>
+                        <li className={ cardData.isPrivateProjects ? "" : "text-muted"}><span className="fa-li"><i className={ cardData.isPrivateProjects ? "fas fa-check" : "fas fa-times"}></i></span>{cardData.privateProjects}</li>
+                        <li className={ cardData.isPhoneSupport ? "" : "text-muted"}><span className="fa-li"><i className={ cardData.isPhoneSupport ? "fas fa-check" : "fas fa-times"}></i></span>{cardData.phoneSupport}</li>
+                        <li className={ cardData.isSubDomian ? "" : "text-muted"}><span className="fa-li"><i className={ cardData.isSubDomian ? "fas fa-check" : "fas fa-times"}></i></span>{cardData.plan === "PRO"  ? <><b>Unlimited</b> {cardData.subDomian}</> : <>{cardData.subDomian}</>}</li>
+                        <li className={ cardData.isReports ? "" : "text-muted"}><span className="fa-li"><i className={ cardData.isReports ? "fas fa-check" : "fas fa-times"}></i></span>{cardData.reports}</li>
                     </ul>
                     <div className="d-grid">
                         <a href="#" className="btn btn-primary text-uppercase">Button</a>
